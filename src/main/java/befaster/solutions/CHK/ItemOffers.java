9e047a8f242b07387a0796899e85603eb195a807
sbx_user1051 <sbx_user1051@169.254.157.205>
2019-04-14 11:16:27 +0000

    Sun Apr 14 11:16:27 UTC 2019

--- src/main/java/befaster/solutions/CHK/ItemOffers.java
+++ src/main/java/befaster/solutions/CHK/ItemOffers.java
@@ -1,5 +1,14 @@
 package befaster.solutions.CHK;
 
-public class ItemOffers {
+import java.util.TreeMap;
 
-}
+public class ItemOffers {
+	
+	public static TreeMap<String, Integer> itemOffers = new TreeMap<String, Integer>();
+	
+	static {
+		itemOffers.put("AAA", 130);
+		itemOffers.put("BB",45);
+	}
+}	
+
