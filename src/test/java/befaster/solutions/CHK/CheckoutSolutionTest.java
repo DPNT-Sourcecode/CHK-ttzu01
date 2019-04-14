3189f491900a467686c59f88a0a13c0319d70359
sbx_user1051 <sbx_user1051@169.254.157.205>
2019-04-14 12:10:27 +0000

    Sun Apr 14 12:10:27 UTC 2019

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -2,13 +2,33 @@ package befaster.solutions.CHK;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class CheckoutSolutionTest {
 
+public class CheckoutSolutionTest {
+	
+	CheckoutSolution chkSolution;
+	@Before
+    public void setUp() {
+
+        chkSolution = new CheckoutSolution();
+    }
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		int expected = 190;
+		int actual = chkSolution.checkout("AABBBD");
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void negativeResult() {
+		int expected = -1;
+		int actual = -1;
+		assertEquals(expected, actual);
 	}
 
 }
+
+
