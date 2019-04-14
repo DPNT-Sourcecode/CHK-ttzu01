d7dd4b5d21dfeaaa70088c6f598ab2950c783ad8
sbx_user1051 <sbx_user1051@169.254.157.205>
2019-04-14 11:49:27 +0000

    Sun Apr 14 11:49:27 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,57 @@
 package befaster.solutions.CHK;
 
+import java.util.TreeMap;
+
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        //throw new SolutionNotImplementedException();
+    	
+    	byte[] items = skus.getBytes();
+    	
+    	int countA=0 , countB=0, countC = 0, countD = 0 ;
+    	for(int i =0 ; i < items.length ; i++) {
+    		switch (items[i]) {
+    		case 'A' : countA++;
+    				 break;
+    		case 'B' : countB++;
+    					break;
+    		case 'C' : countC++;
+			 			break;
+    		case 'D' : countD++;
+						break;	
+			default : return -1;			
+    		}
+    		
+    	}
+    	TreeMap<String, Integer> itemList = new TreeMap<String, Integer>();
+    	itemList.put("A", countA);
+    	itemList.put("B",countB);
+    	itemList.put("C", countC);
+    	itemList.put("D",countD);
+    	
+    	int amount = calculatePrice(itemList);
+    	
+    	
+    	return null;
     }
+
+	private int calculatePrice(TreeMap<String, Integer> itemList) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+    
+	/*
+	 * private int calculatePrice() { // TODO Auto-generated method stub int
+	 * priceOfA = 0; int priceOfB = 0; if(countA > 3 || countB > 2) { priceOfA +=
+	 * (countA/3) * ItemOffers.itemOffers.get("AAA") +
+	 * (countA%3)*ItemPriceList.itemPriceList.get("A"); } if(countB % 2 == 0) {
+	 * priceOfB += (countB/2) * ItemOffers.itemOffers.get("BB"); }
+	 * 
+	 * }
+	 */
+
+
 }
+
--- src/main/java/befaster/solutions/CHK/ItemOffers.java
+++ src/main/java/befaster/solutions/CHK/ItemOffers.java
@@ -3,11 +3,20 @@ package befaster.solutions.CHK;
 import java.util.TreeMap;
 
 public class ItemOffers {
+	String item;
+	Integer quantity;
+	Integer price;
+	public ItemOffers(String item, Integer quantity, Integer price) {
+		this.item=item;
+		this.quantity=quantity;
+		this.price=price;
+	}
 	
-	public static TreeMap<String, Integer> itemOffers = new TreeMap<String, Integer>();
+	public static TreeMap<String, ItemOffers> itemOffers = new TreeMap<String, ItemOffers>();
 	
 	static {
-		itemOffers.put("AAA", 130);
-		itemOffers.put("BB",45);
+		itemOffers.put("A", new ItemOffers("A", 3, 130));
+		itemOffers.put("B", new ItemOffers("B", 2, 45));
 	}
 }	
+
