088fc882f5d59af18a4d965835badfc0de56f505
sbx_user1051 <sbx_user1051@169.254.157.205>
2019-04-14 13:23:34 +0000

    Sun Apr 14 13:23:34 UTC 2019

--- src/main/java/befaster/solutions/CHK/ItemOffers.java
+++ src/main/java/befaster/solutions/CHK/ItemOffers.java
@@ -4,7 +4,7 @@ import java.util.TreeMap;
 
 public class ItemOffers {
 	String item;
-	TreeMap<Integer, Integer> multiOffers = new TreeMap<Integer, Integer>(); 
+	 
 	Integer quantity;
 	Integer price;
 	public ItemOffers(String item, Integer quantity, Integer price) {
@@ -13,13 +13,18 @@ public class ItemOffers {
 		this.price=price;
 	}
 	
+	
+	
 	public static TreeMap<String, ItemOffers> itemOffers = new TreeMap<String, ItemOffers>();
-	 
+	public static TreeMap<Integer, Integer> multiOffers = new TreeMap<Integer, Integer>();
+	public static TreeMap<String, TreeMap> offer = new TreeMap<String, TreeMap>();
 	
 	static {
-		
-		itemOffers.put("A", new ItemOffers("A", 3, 130));
+		multiOffers.put(5,200);
+		multiOffers.put(3,130);
+		offer.put("A", multiOffers);
 		itemOffers.put("B", new ItemOffers("B", 2, 45));
 		itemOffers.put("E", new ItemOffers("E", 2, -30));
 	}
 }	
+
