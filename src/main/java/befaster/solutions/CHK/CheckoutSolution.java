5dfa54e5bfc9f1c5e7bbfbf964c4f016cc257909
sbx_user1051 <sbx_user1051@169.254.157.205>
2019-04-14 12:04:27 +0000

    Sun Apr 14 12:04:27 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,37 +7,37 @@ import java.util.TreeMap;
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
-    public Integer checkout(String skus) {
-        //throw new SolutionNotImplementedException();
-    	
-    	byte[] items = skus.getBytes();
-    	
-    	int countA=0 , countB=0, countC = 0, countD = 0 ;
-    	for(int i =0 ; i < items.length ; i++) {
-    		switch (items[i]) {
-    		case 'A' : countA++;
-    				 break;
-    		case 'B' : countB++;
-    					break;
-    		case 'C' : countC++;
-			 			break;
-    		case 'D' : countD++;
-						break;	
+	public Integer checkout(String skus) {
+		//throw new SolutionNotImplementedException();
+
+		byte[] items = skus.getBytes();
+
+		int countA=0 , countB=0, countC = 0, countD = 0 ;
+		for(int i =0 ; i < items.length ; i++) {
+			switch (items[i]) {
+			case 'A' : countA++;
+			break;
+			case 'B' : countB++;
+			break;
+			case 'C' : countC++;
+			break;
+			case 'D' : countD++;
+			break;	
 			default : return -1;			
-    		}
-    		
-    	}
-    	TreeMap<String, Integer> itemList = new TreeMap<String, Integer>();
-    	itemList.put("A", countA);
-    	itemList.put("B",countB);
-    	itemList.put("C", countC);
-    	itemList.put("D",countD);
-    	
-    	int amount = calculatePrice(itemList);
-    	
-    	
-    	return null;
-    }
+			}
+
+		}
+		TreeMap<String, Integer> itemList = new TreeMap<String, Integer>();
+		itemList.put("A", countA);
+		itemList.put("B",countB);
+		itemList.put("C", countC);
+		itemList.put("D",countD);
+
+		int amount = calculatePrice(itemList);
+
+
+		return amount;
+	}
 
 	private int calculatePrice(TreeMap<String, Integer> itemList) {
 		int total =0 ;
@@ -45,10 +45,10 @@ public class CheckoutSolution {
 		for(Iterator<String> i = keys.iterator(); i.hasNext();) {
 			String item = i.next();
 			Integer quantity = itemList.get(item);
-			
+
 			if(ItemOffers.itemOffers.containsKey(item)) {
 				ItemOffers offer = ItemOffers.itemOffers.get(item);
-				
+
 				if(offer.quantity < quantity) {
 					total += (quantity% offer.quantity)*ItemPriceList.itemPriceList.get(item) + (quantity/offer.quantity)*offer.price;
 				}
@@ -61,19 +61,10 @@ public class CheckoutSolution {
 			}
 		}
 		return total;
-	}
-    
-	/*
-	 * private int calculatePrice() { // TODO Auto-generated method stub int
-	 * priceOfA = 0; int priceOfB = 0; if(countA > 3 || countB > 2) { priceOfA +=
-	 * (countA/3) * ItemOffers.itemOffers.get("AAA") +
-	 * (countA%3)*ItemPriceList.itemPriceList.get("A"); } if(countB % 2 == 0) {
-	 * priceOfB += (countB/2) * ItemOffers.itemOffers.get("BB"); }
-	 * 
-	 * }
-	 */
+	}    
 
 
 }
+
 
 
